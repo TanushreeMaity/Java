02b5ab4225486144f13bd49f225beca7da7328d9
TanushreeMaity <tanushreemaity8298@gmail.com>
2024-05-31 19:23:39 +0530

    1 change

--- Lab4 21STRING/EmailExtractor.java
+++ Lab4 21STRING/EmailExtractor.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class EmailExtractor {
     public static void main(String[] args) {
-        String text = "Hey, there how are you? this is my email id  great123@gmail.com";
+        String text = "Hey, there how are you? this is my email id  Subhamjha@gmail.com";
 
         // Define a regular expression pattern for matching email addresses
         Pattern pattern = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,7}\\b");
